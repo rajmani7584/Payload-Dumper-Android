d49d473eb830a45d68a92407e861cd85f8b70ca0
Rajmani Kumar <rajmani7584@gmail.com>
2026-07-02 01:09:21 +0530

    added back libs.so + initial workflow setup

--- lib/payload-dumper-android-rs/build.sh
+++ lib/payload-dumper-android-rs/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo ndk build -t arm64-v8a -t armeabi-v7a -t x86_64 -t x86 -o ../../app/src/main/jniLibs --release
+cargo ndk build -t arm64-v8a -t armeabi-v7a -o ../../app/src/main/jniLibs --release
